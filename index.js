5a13da629a35c7180583064fdadc4fa490802886
lenhidalgo@gmail.com <lenhidalgo@gmail.com>
2018-04-20 07:32:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // write your code below!
 var name = "Susan"
 var height = 39
-var message = ` is ${height} inches tall`
+var message = ` is ${height} inches tall ${name}`
 /*describe ('Name',()=>{it('returns"Susan"',() => { 
   expect(index.name).toEqual('Susan')
 })})

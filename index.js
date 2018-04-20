b3315aa0d980336fac1141da8e50315e43fbccf1
lenhidalgo@gmail.com <lenhidalgo@gmail.com>
2018-04-20 07:29:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,9 @@ var message = ` is ${height} inches tall`
 describe ('Name',()=>{it('returns"Susan"',() => { 
   expect(index.name).toEqual('Susan')
 })})
+describe ("Height",()=>
+{ it("is lessthan 40",() => {
+  expect(index.height).toBeLessThan(40)})})
+
 module.exports = {name, height, message}
 

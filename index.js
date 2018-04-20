2eb8f14fc039e3de4190535522f54f0c34fe4c8c
lenhidalgo@gmail.com <lenhidalgo@gmail.com>
2018-04-20 07:31:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ describe ('Name',()=>{it('returns"Susan"',() => {
 })})
 describe ("Height",()=>
 { it("is lessthan 40",() => {
-  expect(index.height).toBeLessThan(40)})})})
-
+  expect(index.height).toBeLessThan(40)})})
+describe("Message",()=> {it("gives the name and height", ()=> { 
+  expect (index.message).toInclude(index.name)
+  expect (index.message).toInclude(index.height)
+})})
 module.exports = {name, height, message}
 

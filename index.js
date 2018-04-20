7255c01489abfb175ae803359b4eff2a51b84f16
lenhidalgo@gmail.com <lenhidalgo@gmail.com>
2018-04-20 07:25:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ var height = 74
 var message = ` is ${height} inches tall`
 
 module.exports = {name, height, message}
-console.log(height)
+describe ('Name',()=>{it('returns"Susan"',() => { 
+  expect(index.name).toEqual('Susan')
+})})
